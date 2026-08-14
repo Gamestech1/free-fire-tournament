2533d2cb74c58dbd1b4f4bcd0e904dda5dc84352
Gamestech1 <gptemail250@gmail.com>
2026-08-14 14:05:38 +0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'sb_publishable_hZJVRoZrDjACam9XllkTTg_93DIHSs4';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-console.log("Supabase connected successfully!");
